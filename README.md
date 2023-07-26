2d653d57d749c28f05ec06b5706af14c5eb80e75
Paul Pacheco <paul.pacheco@pragma.com.co>
2023-07-26 15:18:11 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MountainViewUI
+
+A description of this package.
---
+++ Sources/MountainViewUI/MountainViewUI.swift
@@ -0,0 +1,6 @@
+public struct MountainViewUI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
