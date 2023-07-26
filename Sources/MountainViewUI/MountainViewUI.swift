3050bca0aaa4f79442615ddc21641e1b55f7b1a3
Paul Pacheco <paul.pacheco@pragma.com.co>
2023-07-26 17:59:49 -0500

    Added some files work in progress

--- Sources/MountainViewUI/MountainViewUI.swift
+++ Sources/MountainViewUI/MountainViewUI.swift
@@ -1,3 +1,6 @@
+import SwiftUI
+
+@available(iOS 15, *)
 public struct MountainViewUI {
     public private(set) var text = "Hello, World!"
 
---
+++ Sources/MountainViewUI/Utils/MountainColor.swift
@@ -0,0 +1,17 @@
+//
+//  MountainColor.swift
+//  
+//
+//  Created by Paul Pacheco on 26/07/23.
+//
+
+import Foundation
+
+enum MountainColor {
+	case primary
+	case secondary
+	case tertiary
+	case surface
+	case background
+	case error
+}
